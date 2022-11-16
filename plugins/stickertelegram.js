2bb7bf9f068f765bbfbd6ddcaa66dd46333f19ea
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-11-15 19:43:28 -0500

    Update stickertelegram.js

--- plugins/stickertelegram.js
+++ plugins/stickertelegram.js
@@ -1,39 +1,41 @@
-const fetch = require("node-fetch");
-const { MessageType } = require('@adiwajshing/baileys')
-const { sticker } = require('../lib/sticker')
+import { sticker } from '../lib/sticker.js'
+
+import { stickerLine, stickerTelegram } from '@bochilteam/scraper'
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `uhm.. ¿dónde está la URL??\n\Ejemplo:\n${usedPrefix + command} https://t.me/addstickers/namapack`
-    if (!args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) throw `url salah`
-    let packName = args[0].replace("https://t.me/addstickers/", "")
 
-    let gas = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, { method: "GET", headers: { "User-Agent": "GoogleBot" } })
-    if (!gas.ok) throw eror
+    // TODO: add stickerly
 
-    let json = await gas.json()
-    m.reply(`*Total stiker:* ${json.result.stickers.length}
-*Estimado completo:* ${json.result.stickers.length * 1.5} detik`.trim())
+    const isTele = /tele/i.test(command)
 
-    for (let i = 0; i < json.result.stickers.length; i++) {
-        let fileId = json.result.stickers[i].thumb.file_id
+    if (!args[0]) throw `Perintah ini untuk mengambil stiker dari ${isTele ? 'Tele' : 'Line'}\n\nContoh penggunaan:\n${usedPrefix + command} spongebob`
 
-        let gasIn = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${fileId}`)
+    const json = await (isTele ? stickerTelegram : stickerLine)(args[0])
 
-        let jisin = await gasIn.json()
+    m.reply(`
+*Total stiker:* ${(json[0]?.stickers || json).length}
+`.trim())
 
+    for (let data of (json[0]?.stickers || json)) {
 
-        // conn.sendMessage(m.chat, { url: "https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/" + jisin.result.file_path }, MessageType.sticker)
-        let stiker = await sticker(false, "https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/" + jisin.result.file_path, global.packname, global.author)
-        await conn.sendMessage(m.chat, stiker, MessageType.sticker)
+        const stiker = await sticker(false, data.sticker || data, global.packname, global.author)
+
+        await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m).catch(console.error)
+
         await delay(1500)
+
     }
-    m.reply('_*Terminado*_')
+
 }
-handler.help = ['stikertele <url>']
+
+handler.help = ['stikerline <url>']
+
 handler.tags = ['sticker']
-handler.command = /^(stic?kertele(gram)?)$/i
 
-handler.limit = 1
+handler.command = /^(stic?ker(line|tele(gram)?))$/i
 
-module.exports = handler
+handler.limit = true
+
+export default handler
 
 const delay = time => new Promise(res => setTimeout(res, time))
